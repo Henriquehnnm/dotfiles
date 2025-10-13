1a6707f57ff15d72efe70af28f1815d73035b806
Henriquehnnm <henriquesardinha987@gmail.com>
2025-10-13 09:41:45 -0300

    Update lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,33 +1,23 @@
--- Este arquivo configura o autocompletar para várias linguagens usando o LSP (Language Server Protocol).
--- LazyVim já vem com `nvim-cmp` e `mason` configurados, então só precisamos
--- indicar quais servidores de linguagem queremos ativar.
-
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- Python
         pyright = {},
-
-        -- JavaScript e TypeScript
         tsserver = {},
-
-        -- Astro
-        astro = {},
-
-        -- PHP
         intelephense = {},
-
-        -- Shell Script
         bashls = {},
-
-        -- HTML
         html = {},
-
-        -- CSS
         cssls = {},
+        astro = {},
       },
     },
+    config = function(_, opts)
+      for server, config in pairs(opts.servers) do
+        vim.lsp.config(server, config)
+        vim.lsp.enable({ server })
+      end
+    end,
   },
 }
+
