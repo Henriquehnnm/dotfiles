9be002021d7da0a11d11716ddff2ecd77964ca5d
Henriquehnnm <henriquesardinha987@gmail.com>
2025-10-15 08:56:40 -0300

    Add integration with Gemini CLI in nvim

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,5 +1,18 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.keymap.set("n", "<leader>gg", function()
+  local term = require("toggleterm.terminal").Terminal:new({
+    cmd = "gemini",
+    direction = "float",
+    float_opts = {
+      border = "curved",
+    },
+  })
+  term:toggle()
+end, {
+  noremap = true,
+  silent = true,
+  desc = "Toggle Gemini terminal",
+})
